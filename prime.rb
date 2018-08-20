37464f693a9c752f9849c7b41e21ab56ababb90b
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 08:03:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,17 @@
 # Add  code here!
 def prime?(int)
-  if int < 2
-    return false 
-  end 
+#   if int < 2
+#     return false 
+#   end 
   
   
-  (2..int).each do |factor|
-    if int % factor == 0 
-     return false 
-     end 
-   end 
-  true 
-end 
+#   (2..int).each do |factor|
+#     if int % factor == 0 
+#     return false 
+#     end 
+#   end 
+#   true 
+# end 
 prime?(2)
 
 
